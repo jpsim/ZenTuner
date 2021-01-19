07034245c9c5cbb94982623a69933d7ee9a0a17a
JP Simard <jp@jpsim.com>
2021-01-19 09:16:41 -0500

    Initial Commit

---
+++ Shared/ZenTunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenTunerApp.swift
+//  Shared
+//
+//  Created by JP Simard on 1/19/21.
+//
+
+import SwiftUI
+
+@main
+struct ZenTunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

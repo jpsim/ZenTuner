e8f6a8fe0fb92236e912070e2192b6ab383cc5e1
JP Simard <jp@jpsim.com>
2021-06-15 10:42:47 -0400

    Trim more from MicrophonePitchDetector
    And set engine output to a silent node, otherwise the app crashes on
    real iOS devices (but not macOS or the iOS Simulator).
    
    Finally, use the default audio session category of `.soloAmbient`. This
    is a better fit for this kind of app.

--- Packages/MicrophonePitchDetector/Sources/MicrophonePitchDetector/AudioEngine.swift
+++ Packages/MicrophonePitchDetector/Sources/MicrophonePitchDetector/AudioEngine.swift
@@ -75,21 +75,12 @@ final class AudioEngine {
 
     private let _input = InputNode()
 
-    /// Input for microphone or other device is created when this is accessed
-    /// If adjusting AudioKit.Settings, do so before setting up the microphone.
-    /// Setting the .defaultToSpeaker option in AudioKit.Settings.session.setCategory after setting up your mic
-    /// can cause the AVAudioEngine to stop running.
-    var input: InputNode? {
-        if #available(macOS 10.14, *) {
-            guard Bundle.main.object(forInfoDictionaryKey: "NSMicrophoneUsageDescription") != nil else {
-                Log("To use the microphone, you must include the NSMicrophoneUsageDescription in your Info.plist",
-                    type: .error)
-                return nil
-            }
-        }
+    /// Input for microphone is created when this is accessed
+    var input: InputNode {
         if _input.isNotConnected {
             _input.connect(to: self)
             _input.isNotConnected = false
+            self.createSilentOutput()
         }
         return _input
     }
@@ -97,32 +88,25 @@ final class AudioEngine {
     /// Empty initializer
     init() {}
 
-    /// Output node
-    var output: Node? {
-        didSet {
-            // AVAudioEngine doesn't allow the outputNode to be changed while the engine is running
-            let wasRunning = avEngine.isRunning
-            if wasRunning { stop() }
+    /// Start the engine
+    func start() throws {
+        try avEngine.start()
+    }
 
-            // remove the exisiting node if it is present
-            if let node = oldValue {
-                mainMixerNode?.removeInput(node)
-                node.detach()
-                avEngine.outputNode.disconnect(input: node.avAudioNode)
-            }
+    /// Stop the engine
+    func stop() {
+        avEngine.stop()
+    }
 
-            // if non nil, set the main output now
-            if let node = output {
-                avEngine.attach(node.avAudioNode)
+    // MARK: - Private
 
-                // create the on demand mixer if needed
-                createEngineMixer()
-                mainMixerNode?.addInput(node)
-                mainMixerNode?.makeAVConnections()
-            }
+    private func createSilentOutput() {
+        let output = _input
+        avEngine.attach(output.avAudioNode)
 
-            if wasRunning { try? start() }
-        }
+        // create the on demand mixer if needed
+        createEngineMixer()
+        mainMixerNode?.addInput(output)
     }
 
     // simulate the AVAudioEngine.mainMixerNode, but create it ourselves to ensure the
@@ -134,6 +118,7 @@ final class AudioEngine {
         avEngine.attach(mixer.avAudioNode)
         avEngine.connect(mixer.avAudioNode, to: avEngine.outputNode, format: .stereo)
         mainMixerNode = mixer
+        mixer.silenceOutput()
     }
 
     private func removeEngineMixer() {
@@ -143,25 +128,22 @@ final class AudioEngine {
         mixer.detach()
         mainMixerNode = nil
     }
-
-    /// Start the engine
-    func start() throws {
-        if output == nil {
-            Log("🛑 Error: Attempt to start engine with no output.")
-            return
-        }
-        try avEngine.start()
-    }
-
-    /// Stop the engine
-    func stop() {
-        avEngine.stop()
-    }
 }
 
 private extension AVAudioFormat {
     static var stereo: AVAudioFormat {
         AVAudioFormat(standardFormatWithSampleRate: 44_100, channels: 2) ??
             AVAudioFormat()
+    }
+}
+
+private extension Node {
+    func detach() {
+        if let engine = avAudioNode.engine {
+            engine.detach(avAudioNode)
+        }
+        for connection in connections {
+            connection.detach()
+        }
     }
 }

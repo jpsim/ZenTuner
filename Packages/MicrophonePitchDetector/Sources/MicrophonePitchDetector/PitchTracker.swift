de14c2380dba7711457f7a522a786e535bb6bc79
JP Simard <jp@jpsim.com>
2022-02-23 21:53:49 -0500

    fixup! Replace PitchTracker C++ struct with Swift class

--- Packages/MicrophonePitchDetector/Sources/MicrophonePitchDetector/PitchTracker.swift
+++ Packages/MicrophonePitchDetector/Sources/MicrophonePitchDetector/PitchTracker.swift
@@ -1,27 +1,27 @@
 import CMicrophonePitchDetector
 
 final class PitchTracker {
-    private var sp: UnsafeMutablePointer<zt_data>?
+    private var data: UnsafeMutablePointer<zt_data>?
     private var ptrack: UnsafeMutablePointer<zt_ptrack>?
 
     init(sampleRate: Int32, hopSize: Int32, peakCount: Int32) {
-        _ = withUnsafeMutablePointer(to: &sp, zt_create)
-        sp!.pointee.sr = sampleRate
+        _ = withUnsafeMutablePointer(to: &data, zt_create)
+        data!.pointee.sr = sampleRate
         _ = withUnsafeMutablePointer(to: &ptrack, zt_ptrack_create)
-        zt_ptrack_init(sp, ptrack, hopSize, peakCount)
+        zt_ptrack_init(data, ptrack, hopSize, peakCount)
     }
 
     deinit {
         _ = withUnsafeMutablePointer(to: &ptrack, zt_ptrack_destroy)
-        _ = withUnsafeMutablePointer(to: &sp, zt_destroy)
+        _ = withUnsafeMutablePointer(to: &data, zt_destroy)
     }
 
     func getPitch(frames: UnsafeMutablePointer<Float>, count: Int) -> Float? {
         var pitch: Float = 0
         var amplitude: Float = 0
 
-        for i in 0..<count {
-            zt_ptrack_compute(sp, ptrack, frames.advanced(by: i), &pitch, &amplitude)
+        for frame in (0..<count).map({ frames.advanced(by: $0) }) {
+            zt_ptrack_compute(data, ptrack, frame, &pitch, &amplitude)
         }
 
         if amplitude > 0.1 {
